c39941979905a802dc2bd9b59287e27db9467fb2
ryanpsouza <ryanpsouzagit@gmail.com>
2025-07-24 19:28:14 -0300

    CRUD implemented

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
 const livros = [
     {
         "id" : 1,
@@ -12,6 +14,12 @@ const livros = [
     }
 ];
 
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
 app.get("/" , (req, res) => {
     res.status(200).send("Hello World!");
 });
@@ -19,5 +27,27 @@ app.get("/" , (req, res) => {
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros[index]);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro excluido com sucesso");
+})
 
 export default app;
